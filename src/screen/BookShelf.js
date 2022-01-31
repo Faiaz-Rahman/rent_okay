264133cbfe26342800dd6947f447c938d6dbd8df
Faiaz Rahman <faiazrahman70@gmail.com>
2022-02-01 02:00:51 +0600

    BookShelf Screen Created

--- src/screen/BookShelf.js
+++ src/screen/BookShelf.js
@@ -35,6 +35,7 @@ export default function BookShelf({ navigation }) {
 			</ScrollView>
 			{/* Floating Button */}
 			<TouchableHighlight
+				underlayColor={"#FFC0CB"}
 				style={styles.floatingButton}
 				onPress={() => navigation.navigate("addToShelf")}
 			>
